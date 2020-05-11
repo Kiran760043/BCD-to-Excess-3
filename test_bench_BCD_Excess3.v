894bfde265c8d10f6a8d050d298552ccbc5e870b
Kiran Jayarama <kiran760043@.github.com>
2020-05-11 16:50:00 -0400

    Design File

--- test_bench_BCD_Excess3.v
+++ test_bench_BCD_Excess3.v
@@ -1,5 +1,5 @@
 //////////////////////////////////////////////////////////////////////////////////////////
-// Design Name: Binary  to Excess 3 converter
+// Design Name: BCD to Excess 3 converter
 // Engineer: kiran
 // Refence : DIgital Design 5th Edition by Morrios Mano & MIcheal Ciletti (pg 132- pg 133)
 //////////////////////////////////////////////////////////////////////////////////////////
@@ -10,27 +10,27 @@ module test_bench_BCD_Excess3();
         reg  [3:0] in =4'h0;
     //output from DUT
         wire [3:0] out;
-        
-    
+
+
     BCD_Excess_3 DUT (.in(in), .out(out));
-    
+
     initial
         begin
             $display($time,"<< Simulation Results >>");
             $monitor($time,"in = %b, out = %b",in,out);
         end
-        
+
     initial
         begin
            forever begin
               if(in == 4'b1111) begin
-                    $finish;                 
+                    $finish;
                end else begin
                     #2;
                     in = in + 4'h1;
-                    #2;                 
+                    #2;
                end
             end
         end
-    
+
 endmodule
