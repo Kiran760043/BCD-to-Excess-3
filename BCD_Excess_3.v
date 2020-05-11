e76be98cbbbfec86dcc22139ea2f4cc87bf3d23e
Kiran Jayarama <kiran760043@.github.com>
2020-05-11 16:40:14 -0400

    Update BCD_Excess_3.v

--- BCD_Excess_3.v
+++ BCD_Excess_3.v
@@ -11,27 +11,27 @@ module BCD_Excess_3(in, out);
     input  wire [3:0] in;
     output reg  [3:0] out; // change the output data type to reg to use case statements
                            // change the output data type to wire to use assign statements
-    
+
     always@(*)
         begin
             case(in)
-                3'h0 : out <= 3'h3;
-                3'h1 : out <= 3'h4;
-                3'h2 : out <= 3'h5;
-                3'h3 : out <= 3'h6;
-                3'h4 : out <= 3'h7;
-                3'h5 : out <= 3'h8;
-                3'h6 : out <= 3'h9;
-                3'h7 : out <= 3'hA;
-                3'h8 : out <= 3'hB;
-                3'h9 : out <= 3'hC;
-                default : out <= 3'h0;
+                4'h0 : out <= 4'h3;
+                4'h1 : out <= 4'h4;
+                4'h2 : out <= 4'h5;
+                4'h3 : out <= 4'h6;
+                4'h4 : out <= 4'h7;
+                4'h5 : out <= 4'h8;
+                4'h6 : out <= 4'h9;
+                4'h7 : out <= 4'hA;
+                4'h8 : out <= 4'hB;
+                4'h9 : out <= 4'hC;
+                default : out <= 4'h0;
             endcase
-        end  
+        end
 
 //    assign out[0] = ~in[0];
 //    assign out[1] = (in[0] & in[1]) | (~(in[0] | in[1]));
 //    assign out[2] = (in[2] ^ (in[1] | in[0]));
 //    assign out[3] = in[3] | (in[2] & (in[1] | in[0]));
-           
+
 endmodule
